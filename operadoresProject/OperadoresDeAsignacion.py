c4ea026b2d77595b33632565acc3ae1628c0dfa0
TheJuan23 <pablocabrera.p21@gmail.com>
2024-09-26 20:01:05 -0500

    archivo con ejercicios de operadores

---
+++ operadoresProject/OperadoresDeAsignacion.py
@@ -0,0 +1,20 @@
+# ejemplo con operadores de Asignacion
+var0 = 5
+var1 = 12
+var2 = 8
+var3 = 14
+var4 = 6
+var5 = 8
+
+var0 += 3
+var1 -= 3
+var2 *= 3
+var3 /= 3
+var4 %= 3
+var5 //= 3
+print(var0)
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
